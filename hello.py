a536551d7f3937029e8d376b5025d5ecbdacafb0
Ehatch1620 <emilyhatcher12@gmail.com>
2025-08-10 22:59:32 -0400

    Added new message

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 # Emily Hatcher
 # 8/10/2025
 # A simple python program to print a message 
+# Emily Hatcher
+# 8/10/25
+# added new message
 
-print("Hello,World")
+print("Hello,World!")
+
+print ("Welcome to GitHub!") 
